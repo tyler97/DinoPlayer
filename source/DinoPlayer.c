7450744bf14261dabf32096000c3dde733b825dd
tyler97 <ruiztyler97@gmail.com>
2020-05-26 23:36:12 -0500

    just need to add delay and os

--- source/DinoPlayer.c
+++ source/DinoPlayer.c
@@ -37,8 +37,6 @@
 #include "dino.h"
 #include <stdio.h>
 /* TODO: insert other definitions and declarations here. */
-static int LDR_PORT = 0;
-static int SERVO_PORT = 0;
 
 /* Variables for TPM */
 tpm_config_t tpmInfo;
@@ -49,19 +47,29 @@ adc16_config_t adc16ConfigStruct;
 adc16_channel_config_t adc16ChannelConfigStruct;
 
 void InitAll(void);
-void ReadLDR(void);
+uint32_t ReadLDR(void);
+void PressDown(void);
+void Release(void);
 /*
  * @brief   Application entry point.
  */
 int main(void) {
 
-	InitAll();
+ 	InitAll();
 
     while(1) {
 
-    	Release();
+    	//Release();
 
-    	ReadLDR();
+    	uint32_t ldr = ReadLDR();
+    	// printf("%u\n", ldr);
+    	if(ldr >= 750){
+    		Release();
+    	}
+    	else
+    	{
+    		PressDown();
+    	}
     }
     return 0 ;
 }
@@ -76,7 +84,7 @@ void Release(void)
 	TPM_UpdatePwmDutycycle(TPM0, kTPM_Chnl_1, kTPM_EdgeAlignedPwm, 2U);
 }
 
-void ReadLDR(void)
+uint32_t ReadLDR(void)
 {
 
 	uint32_t adc = 0;
@@ -89,9 +97,8 @@ void ReadLDR(void)
 		//wait...
 	}
 	adc = ADC16_GetChannelConversionValue(ADC0, ADC_CHANNEL_GROUP);
-	//printf("%d\n", adc);
 
-	//TPM_UpdatePwmDutycycle(TPM0, kTPM_Chnl_1, kTPM_CenterAlignedPwm, adc * 10);
+	return adc;
 
 }
 void InitAll(void)
